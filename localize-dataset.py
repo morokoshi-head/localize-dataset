48b9c59135b0a8eaf2460d7797df60ed26deadc5
morokoshi-head <ry111000000@gmail.com>
2022-08-21 00:49:29 +0900

    軽微な修正

--- localize-dataset.py
+++ localize-dataset.py
@@ -7,14 +7,14 @@ import sys
 from tqdm import tqdm
 
 
-LOCALIZE_WIDTH  = 480
+LOCALIZE_WIDTH = 480
 LOCALIZE_HEIGHT = 270
 
-input_dir  = ""
+input_dir = ""
 output_dir = ""
 
 
-def lclz_bbox_1d(obj_s, obj_e, crp_s, crp_e, max_size):
+def localize_bbox_1d(obj_s, obj_e, crp_s, crp_e, max_size):
     lcl_obj_s = 0
     lcl_obj_e = 0
 
@@ -50,11 +50,11 @@ def main():
     global output_dir
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("input_dir", help = "input directory")
-    parser.add_argument("output_dir", help = "output directory")
+    parser.add_argument("input_dir", help="input directory")
+    parser.add_argument("output_dir", help="output directory")
 
-    args       = parser.parse_args()
-    input_dir  = args.input_dir
+    args = parser.parse_args()
+    input_dir = args.input_dir
     output_dir = args.output_dir
 
     if(os.path.exists(output_dir)):
@@ -77,26 +77,26 @@ def main():
             lines = rf.readlines()
 
             for line in lines:
-                line  = line.replace("\n", "")
+                line = line.replace("\n", "")
                 value = line.split(" ")
     
                 obj_class = int(value[0])
-                obj_cx    = float(value[1]) * img_w
-                obj_cy    = float(value[2]) * img_h
-                obj_w     = float(value[3]) * img_w
-                obj_h     = float(value[4]) * img_h
+                obj_cx = float(value[1]) * img_w
+                obj_cy = float(value[2]) * img_h
+                obj_w  = float(value[3]) * img_w
+                obj_h  = float(value[4]) * img_h
     
-                obj_sx = obj_cx - (obj_w / 2.0)
-                obj_ex = obj_cx + (obj_w / 2.0)
-                obj_sy = obj_cy - (obj_h / 2.0)
-                obj_ey = obj_cy + (obj_h / 2.0)
+                obj_sx = obj_cx - (obj_w/2.0)
+                obj_ex = obj_cx + (obj_w/2.0)
+                obj_sy = obj_cy - (obj_h/2.0)
+                obj_ey = obj_cy + (obj_h/2.0)
     
                 lcl_img_num = 0
                 for i in range(crp_num_y):
                     crp_sy = LOCALIZE_HEIGHT * i
                     crp_ey = crp_sy + LOCALIZE_HEIGHT
     
-                    lcl_obj_sy, lcl_obj_ey = lclz_bbox_1d(obj_sy, obj_ey, crp_sy, crp_ey, LOCALIZE_HEIGHT)
+                    lcl_obj_sy, lcl_obj_ey = localize_bbox_1d(obj_sy, obj_ey, crp_sy, crp_ey, LOCALIZE_HEIGHT)
     
                     for j in range(crp_num_x):
                         lcl_img_num += 1
@@ -107,9 +107,9 @@ def main():
                         lcl_obj_sx = 0
                         lcl_obj_ex = LOCALIZE_WIDTH - 1
     
-                        lcl_obj_sx, lcl_obj_ex = lclz_bbox_1d(obj_sx, obj_ex, crp_sx, crp_ex, LOCALIZE_WIDTH)
+                        lcl_obj_sx, lcl_obj_ex = localize_bbox_1d(obj_sx, obj_ex, crp_sx, crp_ex, LOCALIZE_WIDTH)
     
-                        lcl_img      = img[crp_sy:crp_ey, crp_sx:crp_ex]
+                        lcl_img = img[crp_sy:crp_ey, crp_sx:crp_ex]
                         lcl_img_name = os.path.basename(img_path).replace(".png", ("_" + str(lcl_img_num) + ".png"))
     
                         cv2.imwrite(os.path.join(output_dir, lcl_img_name), lcl_img)
@@ -118,13 +118,12 @@ def main():
                             if(((lcl_obj_ey - lcl_obj_sy) == 0) or ((lcl_obj_ey - lcl_obj_sy) == 0)):
                                 pass
                             else:
-                                print(str(obj_class), " ",
-                                      str((lcl_obj_sx + lcl_obj_ex) / (LOCALIZE_WIDTH * 2)), " ",
-                                      str((lcl_obj_sy + lcl_obj_ey) / (LOCALIZE_HEIGHT * 2)), " ",
-                                      str((lcl_obj_ex - lcl_obj_sx) / LOCALIZE_WIDTH), " ",
-                                      str((lcl_obj_ey - lcl_obj_sy) / LOCALIZE_HEIGHT), file=wf)
-
-        sys.exit()
+                                print(str(obj_class), " ", \
+                                      str((lcl_obj_sx+lcl_obj_ex) / (LOCALIZE_WIDTH*2)), " ", \
+                                      str((lcl_obj_sy+lcl_obj_ey) / (LOCALIZE_HEIGHT*2)), " ", \
+                                      str((lcl_obj_ex-lcl_obj_sx) / LOCALIZE_WIDTH), " ", \
+                                      str((lcl_obj_ey-lcl_obj_sy) / LOCALIZE_HEIGHT), file=wf)
     
     
-main()
+if __name__ == "__main__":
+    main()
